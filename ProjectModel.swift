1bad7acd5bec28a4bd1c9737032ddc6c6e0fd775
jakecolacoo <94580156+jakecolacoo@users.noreply.github.com>
2025-08-15 15:25:25 -0600

    Create ProjectModel.swift

---
+++ ProjectModel.swift
@@ -0,0 +1,12 @@
+
+// Handles:
+
+// Saving/loading .gpsproj project files (JSON)
+
+// Storing:
+
+// Paths to sample files
+
+// Track sensitivity/volume/pan
+
+// Recorded patterns (if you add overdubbing later)
