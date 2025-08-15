494bb6054416db764b3f21028964aad0c3648dcf
jakecolacoo <94580156+jakecolacoo@users.noreply.github.com>
2025-08-15 15:25:00 -0600

    Create TrackManager.swift

---
+++ TrackManager.swift
@@ -0,0 +1,7 @@
+// Handles:
+
+// Keeping a list of up to 12 tracks
+
+// Each track’s assigned sample file, sensitivity, and output volume
+
+// Triggering sample playback when TransientDetector says “hit”
