580702dd812574ee0053d025b084297f0e1ebc55
jakecolacoo <94580156+jakecolacoo@users.noreply.github.com>
2025-08-15 15:23:48 -0600

    Create AudioEngine.swift

---
+++ AudioEngine.swift
@@ -0,0 +1,9 @@
+// Handles:
+
+// Setting up Core Audio (via AVAudioEngine for simplicity)
+
+// Capturing the guitar input
+
+// Sending it through the transient detector
+
+// Playing back triggered percussion samples
